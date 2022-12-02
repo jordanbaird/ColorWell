3b6a80d24d7dc44a40f0f83e32ffa4eef9cc9c5f
Jordan Baird <jordanbaird.dev@gmail.com>
2022-12-02 07:52:19 -0700

    API tweaks
    * Internal tweaks
    * Minor external improvements
    * Several new initializers in `ColorWellView`
    * Better documentation

--- Sources/ColorWell/ColorWellView.swift
+++ Sources/ColorWell/ColorWellView.swift
@@ -9,103 +9,164 @@
 #if canImport(SwiftUI)
 import SwiftUI
 
-@available(macOS 10.15, *)
-private struct _ColorWellView: NSViewRepresentable {
-  typealias NSViewType = ColorWell
+// MARK: ColorWellWrapper
 
-  let constructor: () -> ColorWell
+@available(macOS 10.15, *)
+private struct ColorWellWrapper: NSViewRepresentable {
+  let color: NSColor?
 
   func makeNSView(context: Context) -> ColorWell {
-    constructor()
+    if let color {
+      return .init(color: color)
+    } else {
+      return .init()
+    }
   }
 
   func updateNSView(_ nsView: ColorWell, context: Context) {
-    nsView.changeHandlers.formUnion(context.environment.colorWellTransformedActions)
+    nsView.changeHandlers.formUnion(context.environment.changeHandlers)
     nsView.isEnabled = context.environment.isEnabled
 
     if #available(macOS 11.0, *) {
-      nsView.swatchColors = context.environment.colorWellSwatchColors
+      nsView.swatchColors = context.environment.swatchColors
     }
   }
 }
 
 @available(macOS 10.15, *)
 public struct ColorWellView: View {
-  let color: NSColor?
+  private let bodyConstructor: () -> any View
+
+  public var body: some View {
+    AnyView(bodyConstructor())
+  }
+
+  private init(
+    color: NSColor?,
+    modifier: @escaping (ColorWellWrapper) -> some View = \.self
+  ) {
+    bodyConstructor = {
+      modifier(ColorWellWrapper(color: color))
+    }
+  }
 
   /// Creates a color well with the default color.
   public init() {
-    color = nil
+    self.init(color: nil)
+  }
+
+  /// Creates a color well that executes the given action when its color changes.
+  ///
+  /// This initializer has the same effect as the ``onColorChange(perform:)`` modifier.
+  ///
+  /// - Parameter action: An action to perform when the color well's color changes.
+  public init(action: @escaping (Color) -> Void) {
+    self.init(color: nil) {
+      $0.onColorChange(perform: action)
+    }
   }
 
   /// Creates a color well view with the given color.
+  ///
+  /// - Parameter color: The starting value of the color well's color.
   @available(macOS 11.0, *)
   public init(color: Color) {
-    self.color = .init(color)
+    self.init(color: .init(color))
+  }
+
+  /// Creates a color well view with the given color and action.
+  ///
+  /// This initializer has the same effect as the ``onColorChange(perform:)`` modifier.
+  ///
+  /// - Parameters:
+  ///   - color: The starting value of the color well's color.
+  ///   - action: An action to perform when the color well's color changes.
+  @available(macOS 11.0, *)
+  public init(color: Color, action: @escaping (Color) -> Void) {
+    self.init(color: .init(color)) {
+      $0.onColorChange(perform: action)
+    }
   }
 
   /// Creates a color well view with the given CoreGraphics color.
+  ///
+  /// - Parameter cgColor: The starting value of the color well's color.
   public init(cgColor: CGColor) {
-    color = .init(cgColor: cgColor)
+    self.init(color: .init(cgColor: cgColor))
   }
 
-  public var body: some View {
-    _ColorWellView {
-      if let color {
-        return ColorWell(color: color)
-      } else {
-        return ColorWell()
-      }
+  /// Creates a color well view with the given CoreGraphics color.
+  ///
+  /// This initializer has the same effect as the ``onColorChange(perform:)`` modifier.
+  ///
+  /// - Note: The color well's color is translated into a `CGColor` from
+  ///   an underlying representation. In some cases, the translation process
+  ///   may be forced to return an approximation, rather than the original
+  ///   color. To receive a color that is guaranteed to be equivalent to the
+  ///   color well's underlying representation, use ``init(color:action:)``.
+  ///
+  /// - Parameters:
+  ///   - cgColor: The starting value of the color well's color.
+  ///   - action: An action to perform when the color well's color changes.
+  public init(cgColor: CGColor, action: @escaping (CGColor) -> Void) {
+    self.init(color: .init(cgColor: cgColor)) {
+      $0.modifier(OnColorChange(action: action))
     }
   }
 }
 
+// MARK: Environment Keys
+
 @available(macOS 10.15, *)
-private struct ColorWellTransformedActionsKey: EnvironmentKey {
+private struct ChangeHandlersKey: EnvironmentKey {
   static let defaultValue = Set<ChangeHandler>()
 }
 
 @available(macOS 11.0, *)
-private struct ColorWellSwatchColorsKey: EnvironmentKey {
-  static let defaultValue = [NSColor]()
+private struct SwatchColorsKey: EnvironmentKey {
+  static let defaultValue = ColorWell.defaultSwatchColors
 }
+
+// MARK: Environment Values
 
 @available(macOS 10.15, *)
 private extension EnvironmentValues {
-  var colorWellTransformedActions: Set<ChangeHandler> {
-    get { self[ColorWellTransformedActionsKey.self] }
-    set { self[ColorWellTransformedActionsKey.self] = newValue }
+  var changeHandlers: Set<ChangeHandler> {
+    get { self[ChangeHandlersKey.self] }
+    set { self[ChangeHandlersKey.self] = newValue }
   }
 }
 
 @available(macOS 11.0, *)
 private extension EnvironmentValues {
-  var colorWellSwatchColors: [NSColor] {
-    get { self[ColorWellSwatchColorsKey.self] }
-    set { self[ColorWellSwatchColorsKey.self] = newValue }
+  var swatchColors: [NSColor] {
+    get { self[SwatchColorsKey.self] }
+    set { self[SwatchColorsKey.self] = newValue }
   }
 }
 
+// MARK: View Modifiers
+
 @available(macOS 10.15, *)
-private struct ColorWellAction: ViewModifier {
+private struct OnColorChange<C: NSColorConvertible<C>>: ViewModifier {
   let id = ComparableID()
-  let action: (Color) -> Void
+  let action: (C.Converted) -> Void
 
   var transformedAction: ChangeHandler {
     ChangeHandler(id: id) {
-      action(Color($0))
+      action(C.converted(from: $0))
     }
   }
 
   func body(content: Content) -> some View {
-    content.transformEnvironment(\.colorWellTransformedActions) {
+    content.transformEnvironment(\.changeHandlers) {
       $0.insert(transformedAction)
     }
   }
 }
 
 @available(macOS 11.0, *)
-private struct ColorWellSwatchColors: ViewModifier {
+private struct SwatchColors: ViewModifier {
   let colors: [Color]
 
   var transformedColors: [NSColor] {
@@ -113,15 +174,17 @@ private struct ColorWellSwatchColors: ViewModifier {
   }
 
   func body(content: Content) -> some View {
-    content.environment(\.colorWellSwatchColors, transformedColors)
+    content.environment(\.swatchColors, transformedColors)
   }
 }
+
+// MARK: View Extensions
 
 @available(macOS 10.15, *)
 extension View {
   /// Adds an action to perform when a color well's color changes.
   public func onColorChange(perform action: @escaping (Color) -> Void) -> some View {
-    modifier(ColorWellAction(action: action))
+    modifier(OnColorChange(action: action))
   }
 }
 
@@ -139,7 +202,42 @@ extension View {
   ///
   /// - Parameter colors: The swatch colors to use.
   public func swatchColors(_ colors: [Color]) -> some View {
-    modifier(ColorWellSwatchColors(colors: colors))
+    modifier(SwatchColors(colors: colors))
+  }
+
+  /// Applies the given swatch colors to the view's color wells.
+  ///
+  /// Swatches are user-selectable colors that are shown when
+  /// a ``ColorWellView`` displays its popover.
+  ///
+  /// ![Default swatches](color-well-with-popover)
+  ///
+  /// Any color well that is part of the current view's hierarchy
+  /// will update its swatches to the colors provided here.
+  ///
+  /// - Parameter colors: The swatch colors to use.
+  public func swatchColors(_ colors: Color...) -> some View {
+    swatchColors(colors)
+  }
+}
+
+// MARK: - NSColorConvertible
+
+private protocol NSColorConvertible<Converted> {
+  associatedtype Converted: NSColorConvertible
+  static func converted(from nsColor: NSColor) -> Converted
+}
+
+@available(macOS 10.15, *)
+extension Color: NSColorConvertible {
+  fileprivate static func converted(from nsColor: NSColor) -> Self {
+    .init(nsColor)
+  }
+}
+
+extension CGColor: NSColorConvertible {
+  fileprivate static func converted(from nsColor: NSColor) -> CGColor {
+    nsColor.cgColor
   }
 }
 #endif
