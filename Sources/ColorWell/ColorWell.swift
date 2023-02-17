24e1be65e267f15a13a0dc3c3ac32e045e886c8a
Jordan Baird <jordanbaird.dev@gmail.com>
2023-02-17 08:50:40 -0700

    Move deprecations into their own file
    Remove redundant deprecation messages

--- Sources/ColorWell/ColorWell.swift
+++ Sources/ColorWell/ColorWell.swift
@@ -307,10 +307,9 @@ public class ColorWell: _ColorWellBaseView {
         get {
             _colorPanel
         }
-        @available(
-            *, deprecated, message:
-            "Only the 'shared' instance of NSColorPanel is valid. Creation of additional instances causes memory leaks."
-        )
+        // Ideally, this would go in Deprecated.swift, but there isn't a way to make
+        // separate getter and setter declarations.
+        @available(*, deprecated, message: "Only the 'shared' instance of NSColorPanel is valid. Creation of additional instances causes memory leaks.")
         set {
             _colorPanel = newValue
         }
@@ -634,51 +633,6 @@ extension ColorWell {
         default:
             return false
         }
-    }
-}
-
-// MARK: ColorWell Deprecated
-extension ColorWell {
-    /// A Boolean value that indicates whether the color well's color panel
-    /// allows adjusting the selected color's opacity.
-    @available(*, deprecated, message: "Renamed to 'showsAlpha'", renamed: "showsAlpha")
-    @objc dynamic
-    public var supportsOpacity: Bool {
-        get { showsAlpha }
-        set { showsAlpha = newValue }
-    }
-
-    /// Activates the color well and displays its color panel.
-    ///
-    /// Both elements will remain synchronized until either the color panel
-    /// is closed, or the color well is deactivated.
-    ///
-    /// - Parameter exclusive: If this value is `true`, all other active
-    ///   color wells attached to this color well's color panel will be
-    ///   deactivated.
-    @available(*, deprecated, message: "Renamed to 'activate(exclusive:)'", renamed: "activate(exclusive:)")
-    public func activate(_ exclusive: Bool) {
-        activate(exclusive: exclusive)
-    }
-
-    /// Adds an action to perform when the color well's color changes.
-    ///
-    /// ```swift
-    /// let colorWell = ColorWell()
-    /// let textView = NSTextView()
-    /// // ...
-    /// // ...
-    /// // ...
-    /// colorWell.observeColor { color in
-    ///     textView.textColor = color
-    /// }
-    /// ```
-    ///
-    /// - Parameter handler: A block of code that will be executed when
-    ///   the color well's color changes.
-    @available(*, deprecated, message: "Renamed to 'onColorChange(perform:)'", renamed: "onColorChange(perform:)")
-    public func observeColor(onChange handler: @escaping (NSColor) -> Void) {
-        onColorChange(perform: handler)
     }
 }
 
---
+++ Sources/ColorWell/Deprecated.swift
@@ -0,0 +1,53 @@
+//===----------------------------------------------------------------------===//
+//
+// Deprecated.swift
+//
+//===----------------------------------------------------------------------===//
+
+import Cocoa
+
+// MARK: - ColorWell
+
+extension ColorWell {
+    /// A Boolean value that indicates whether the color well's color panel
+    /// allows adjusting the selected color's opacity.
+    @available(*, deprecated, renamed: "showsAlpha")
+    @objc dynamic
+    public var supportsOpacity: Bool {
+        get { showsAlpha }
+        set { showsAlpha = newValue }
+    }
+
+    /// Activates the color well and displays its color panel.
+    ///
+    /// Both elements will remain synchronized until either the color panel
+    /// is closed, or the color well is deactivated.
+    ///
+    /// - Parameter exclusive: If this value is `true`, all other active
+    ///   color wells attached to this color well's color panel will be
+    ///   deactivated.
+    @available(*, deprecated, renamed: "activate(exclusive:)")
+    public func activate(_ exclusive: Bool) {
+        activate(exclusive: exclusive)
+    }
+
+    /// Adds an action to perform when the color well's color changes.
+    ///
+    /// ```swift
+    /// let colorWell = ColorWell()
+    /// let textView = NSTextView()
+    /// // ...
+    /// // ...
+    /// // ...
+    /// colorWell.observeColor { color in
+    ///     textView.textColor = color
+    /// }
+    /// ```
+    ///
+    /// - Parameter handler: A block of code that will be executed when
+    ///   the color well's color changes.
+    @available(*, deprecated, renamed: "onColorChange(perform:)")
+    public func observeColor(onChange handler: @escaping (NSColor) -> Void) {
+        onColorChange(perform: handler)
+    }
+}
