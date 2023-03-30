92e9508ff9683dc607b49f530d153497939fdeb9
Jordan Baird <jordanbaird.dev@gmail.com>
2023-03-30 11:58:31 -0600

    Use `Cache` object to cache image layer

--- Sources/ColorWell/Views/Cocoa/Segments/ColorWellToggleSegment.swift
+++ Sources/ColorWell/Views/Cocoa/Segments/ColorWellToggleSegment.swift
@@ -14,8 +14,7 @@ class ColorWellToggleSegment: ColorWellSegment {
 
     // MARK: Instance Properties
 
-    // FIXME: This should be an actual Cache object.
-    private var cachedImageLayer: CALayer?
+    private var cachedImageLayer = Cache(CALayer(), id: _ImageLayerCacheID())
 
     override var side: Side { .right }
 
@@ -38,6 +37,93 @@ class ColorWellToggleSegment: ColorWellSegment {
         // segment to fill the remaining space.
         translatesAutoresizingMaskIntoConstraints = false
         widthAnchor.constraint(equalToConstant: Self.widthConstant).isActive = true
+
+        cachedImageLayer.updateConstructor { id in
+            enum LocalCache {
+                private static let defaultImage: NSImage = {
+                    // Force unwrap is okay here, as the image is an AppKit builtin.
+                    // swiftlint:disable:next force_unwrapping
+                    let image = NSImage(named: NSImage.touchBarColorPickerFillName)!
+                    return image.clippedToSquare()
+                }()
+
+                static let defaultContents = {
+                    let scale = defaultImage.recommendedLayerContentsScale(0.0)
+                    return defaultImage.layerContents(forContentsScale: scale)
+                }()
+
+                static let enabledTintedForDarkAppearance = {
+                    let image = defaultImage.tinted(to: .white, amount: 0.33)
+                    let scale = image.recommendedLayerContentsScale(0.0)
+                    return image.layerContents(forContentsScale: scale)
+                }()
+
+                static let enabledTintedForLightAppearance = {
+                    let image = defaultImage.tinted(to: .black, amount: 0.20)
+                    let scale = image.recommendedLayerContentsScale(0.0)
+                    return image.layerContents(forContentsScale: scale)
+                }()
+
+                static let disabledTintedForDarkAppearance = {
+                    let image = NSImage(size: defaultImage.size, flipped: false) { bounds in
+                        defaultImage
+                            .tinted(to: .gray, amount: 0.33)
+                            .draw(in: bounds, from: bounds, operation: .copy, fraction: 0.5)
+                        return true
+                    }
+                    let scale = image.recommendedLayerContentsScale(0.0)
+                    return image.layerContents(forContentsScale: scale)
+                }()
+
+                static let disabledTintedForLightAppearance = {
+                    let image = NSImage(size: defaultImage.size, flipped: false) { bounds in
+                        defaultImage
+                            .tinted(to: .gray, amount: 0.20)
+                            .draw(in: bounds, from: bounds, operation: .copy, fraction: 0.5)
+                        return true
+                    }
+                    let scale = image.recommendedLayerContentsScale(0.0)
+                    return image.layerContents(forContentsScale: scale)
+                }()
+
+                static func tintedForDarkAppearance(_ isEnabled: Bool) -> Any {
+                    guard isEnabled else {
+                        return disabledTintedForDarkAppearance
+                    }
+                    return enabledTintedForDarkAppearance
+                }
+
+                static func tintedForLightAppearance(_ isEnabled: Bool) -> Any {
+                    guard isEnabled else {
+                        return disabledTintedForLightAppearance
+                    }
+                    return enabledTintedForLightAppearance
+                }
+            }
+
+            let dimension = min(id.bounds.width, id.bounds.height) - 6
+            let imageLayer = CALayer()
+
+            imageLayer.frame = NSRect(
+                x: 0,
+                y: 0,
+                width: dimension,
+                height: dimension
+            ).centered(in: id.bounds)
+
+            if id.state == .highlight || !id.isEnabled {
+                switch DrawingStyle.current {
+                case .dark:
+                    imageLayer.contents = LocalCache.tintedForDarkAppearance(id.isEnabled)
+                case .light:
+                    imageLayer.contents = LocalCache.tintedForLightAppearance(id.isEnabled)
+                }
+            } else {
+                imageLayer.contents = LocalCache.defaultContents
+            }
+
+            return imageLayer
+        }
     }
 }
 
@@ -46,99 +132,12 @@ extension ColorWellToggleSegment {
     /// Adds a layer that contains an image indicating that the
     /// segment toggles the color panel.
     private func updateImageLayer(_ dirtyRect: NSRect) {
-        enum LocalCache {
-            private static let defaultImage: NSImage = {
-                // Force unwrap is okay here, as the image is an AppKit builtin.
-                // swiftlint:disable:next force_unwrapping
-                let image = NSImage(named: NSImage.touchBarColorPickerFillName)!
-                return image.clippedToSquare()
-            }()
-
-            static let defaultContents = {
-                let scale = defaultImage.recommendedLayerContentsScale(0.0)
-                return defaultImage.layerContents(forContentsScale: scale)
-            }()
-
-            static let enabledTintedForDarkAppearance = {
-                let image = defaultImage.tinted(to: .white, amount: 0.33)
-                let scale = image.recommendedLayerContentsScale(0.0)
-                return image.layerContents(forContentsScale: scale)
-            }()
-
-            static let enabledTintedForLightAppearance = {
-                let image = defaultImage.tinted(to: .black, amount: 0.20)
-                let scale = image.recommendedLayerContentsScale(0.0)
-                return image.layerContents(forContentsScale: scale)
-            }()
-
-            static let disabledTintedForDarkAppearance = {
-                let image = NSImage(size: defaultImage.size, flipped: false) { bounds in
-                    defaultImage
-                        .tinted(to: .gray, amount: 0.33)
-                        .draw(in: bounds, from: bounds, operation: .copy, fraction: 0.5)
-                    return true
-                }
-                let scale = image.recommendedLayerContentsScale(0.0)
-                return image.layerContents(forContentsScale: scale)
-            }()
-
-            static let disabledTintedForLightAppearance = {
-                let image = NSImage(size: defaultImage.size, flipped: false) { bounds in
-                    defaultImage
-                        .tinted(to: .gray, amount: 0.20)
-                        .draw(in: bounds, from: bounds, operation: .copy, fraction: 0.5)
-                    return true
-                }
-                let scale = image.recommendedLayerContentsScale(0.0)
-                return image.layerContents(forContentsScale: scale)
-            }()
-
-            static func tintedForDarkAppearance(_ isEnabled: Bool) -> Any {
-                guard isEnabled else {
-                    return disabledTintedForDarkAppearance
-                }
-                return enabledTintedForDarkAppearance
-            }
-
-            static func tintedForLightAppearance(_ isEnabled: Bool) -> Any {
-                guard isEnabled else {
-                    return disabledTintedForLightAppearance
-                }
-                return enabledTintedForLightAppearance
-            }
-        }
-
-        // FIXME: This should be an actual Cache object (see above).
-        cachedImageLayer?.removeFromSuperlayer()
-        cachedImageLayer = nil
-
+        cachedImageLayer.cachedValue.removeFromSuperlayer()
         guard let layer else {
             return
         }
-
-        let dimension = min(dirtyRect.width, dirtyRect.height) - 6
-        let imageLayer = CALayer()
-
-        imageLayer.frame = NSRect(
-            x: 0,
-            y: 0,
-            width: dimension,
-            height: dimension
-        ).centered(in: dirtyRect)
-
-        if state == .highlight || !isEnabled {
-            switch DrawingStyle.current {
-            case .dark:
-                imageLayer.contents = LocalCache.tintedForDarkAppearance(isEnabled)
-            case .light:
-                imageLayer.contents = LocalCache.tintedForLightAppearance(isEnabled)
-            }
-        } else {
-            imageLayer.contents = LocalCache.defaultContents
-        }
-
-        layer.addSublayer(imageLayer)
-        cachedImageLayer = imageLayer
+        cachedImageLayer.recache(id: _ImageLayerCacheID(dirtyRect, self))
+        layer.addSublayer(cachedImageLayer.cachedValue)
     }
 }
 
@@ -194,5 +193,27 @@ extension ColorWellToggleSegment {
 extension ColorWellToggleSegment {
     override func accessibilityLabel() -> String? {
         "color picker"
+    }
+}
+
+extension ColorWellToggleSegment {
+    private struct _ImageLayerCacheID: Equatable {
+        let bounds: NSRect
+        let state: State
+        let isEnabled: Bool
+
+        init(bounds: NSRect, state: State, isEnabled: Bool) {
+            self.bounds = bounds
+            self.state = state
+            self.isEnabled = isEnabled
+        }
+
+        init(_ bounds: NSRect, _ segment: ColorWellToggleSegment) {
+            self.init(bounds: bounds, state: segment.state, isEnabled: segment.isEnabled)
+        }
+
+        init() {
+            self.init(bounds: .zero, state: .default, isEnabled: true)
+        }
     }
 }
