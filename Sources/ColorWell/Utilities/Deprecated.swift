5622821ee8d7d90c2811feddb6e747425d0029c9
Jordan Baird <jordanbaird.dev@gmail.com>
2023-03-31 22:25:18 -0600

    A mark

--- Sources/ColorWell/Utilities/Deprecated.swift
+++ Sources/ColorWell/Utilities/Deprecated.swift
@@ -5,6 +5,8 @@
 
 import Cocoa
 
+// MARK: - ColorWell
+
 extension ColorWell {
     /// The color panel associated with the color well.
     @available(*, deprecated, message: "'colorPanel' is no longer used and will be removed in a future release. Use 'NSColorPanel.shared' instead.")
